90c506c2c0279356697065ca0e136b58a91b38fd
stevenc12 <mr.kevin.s.cheng@gmail.com>
2023-05-14 20:30:58 -0400

    summaries and test csv

--- loader.py
+++ loader.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
+""" CSV Column Statistics
+
+For each column of a CSV, prints:
+- (min, lower quantile, median, upper quantile, max)
+- (mean, sd, skewness, kurtosis)
+- Likely distribution from (binomial, geometric, Poisson, exponential, Gaussian, multinomial)
+
+Accepts .csv files.
+"""
 import sys
+import pandas as pd
+
+def summarize_csv(file_name : str) -> None:
+    data = pd.read_csv(file_name)
+    for i in range(0,2):#len(data.columns.values)):
+        print(data.columns.values[i])
+        col = data.iloc[:,i]
+        if col.dtype == 'int64' or col.dtype == 'float64':
+            print(data.iloc[:,i].describe())
+
 
 if __name__ == "__main__":
-    print(sys.argv)
+    if len(sys.argv) < 2:
+        print("Usage: Please specify an Excel File to summarize.")
+        exit(1)
+    summarize_csv(sys.argv[1])
